f2b6f81a0426278bba492a541b00beca57a5e884
tini <martynas.raiselis@gmail.com>
2025-03-13 15:01:58 +0100

    chore: remove unnecessary files

--- src/csv-parser/csv-parser.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CsvParserService } from './csv-parser.service';
-
-@Module({
-    providers: [CsvParserService],
-})
-export class CsvParserModule { }
