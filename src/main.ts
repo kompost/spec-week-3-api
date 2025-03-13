f0ec13edacd78afb8597c906e132d0e389fcaf3d
tini <martynas.raiselis@gmail.com>
2025-03-13 15:00:53 +0100

    feat(swagger): add swagger module to the app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('v1');
+
+    const rootConfig = new DocumentBuilder()
+        .setTitle('Products API - week 3')
+        .setDescription('Nice')
+        .setVersion('1.0')
+        .addTag('Spec API')
+        .build();
+
+    const rootDocument = SwaggerModule.createDocument(app, rootConfig);
+    SwaggerModule.setup('api', app, rootDocument);
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
